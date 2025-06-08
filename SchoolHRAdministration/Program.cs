34dd7e044e036fc1f633b9536ad63b60365173e3
Luong <luongvq004@gmail.com>
2025-06-09 01:55:47 +0700

    base start common

--- SchoolHRAdministration/Program.cs
+++ SchoolHRAdministration/Program.cs
@@ -1,104 +1,14 @@
-﻿using HRAdministrationAPI;
-
-
-namespace SchoolHRAdministration
+﻿namespace SchoolHRAdministration
 {
-    public enum EmployeeType
-    {
-        Teacher, HeadOfDepartment, DeputyHeadMaster, HeadMaster
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            List<IEmployee> employees  = new List<IEmployee>();
-
-            seedData(employees);
-            Console.WriteLine($"Total Salary: {employees.Sum(e => e.Salary)}");
-            Console.ReadKey();
+            
         }
 
-        public static void seedData(List<IEmployee> employees)
-        {
-            IEmployee teacher1 = EmployeeFactory.GetEmployeeInstance(EmployeeType.Teacher, 1, "Long", "Khong", 40000);
-            employees.Add(teacher1);
-
-            IEmployee teacher2 = EmployeeFactory.GetEmployeeInstance(EmployeeType.Teacher, 2, "Trung", "Ko", 30000);
-            employees.Add(teacher2);
-
-            IEmployee headOfDepartment = EmployeeFactory.GetEmployeeInstance(EmployeeType.HeadOfDepartment, 3, "Giang", "Phan", 90000);
-            employees.Add(headOfDepartment);
-
-            IEmployee deputyHeadMaster = EmployeeFactory.GetEmployeeInstance(EmployeeType.DeputyHeadMaster, 4, "Quan", "Vu", 14000);
-            employees.Add(deputyHeadMaster);
-
-            IEmployee headMaster = EmployeeFactory.GetEmployeeInstance(EmployeeType.HeadMaster, 5, "Tong", "Giang", 90400);
-            employees.Add(headMaster);
-        }
+       
     }
 
-    public class Teacher : EmployeeBase
-    {
-        public override decimal Salary { get => base.Salary + (base.Salary * 0.02m); }
-    }
 
-    public class HeadOfDepartment : EmployeeBase
-    {
-        public override decimal Salary { get => base.Salary + (base.Salary * 0.03m); }
-
-    }
-
-    public class DeputyHeadMaster : EmployeeBase
-    {
-        public override decimal Salary { get => base.Salary + (base.Salary * 0.04m); }
-    }
-
-    public class HeadMaster : EmployeeBase
-    {
-        public override decimal Salary { get => base.Salary + (base.Salary * 0.05m); }
-    }
-
-    public static class EmployeeFactory
-    {
-        public static IEmployee GetEmployeeInstance (EmployeeType employeeType, int id, string firstName, string lastName, decimal salary)
-        {
-            IEmployee employee = null;
-
-            switch (employeeType)
-            {
-                case EmployeeType.Teacher:
-                    employee = FactoryPattern<IEmployee, Teacher>.GetInstance();
-                        //new Teacher { Id = id, FirstName = firstName, LastName = lastName, Salary = salary };
-                    break;
-                case EmployeeType.HeadOfDepartment:
-                    employee = FactoryPattern<IEmployee, HeadOfDepartment>.GetInstance();
-                        //new HeadOfDepartment { Id = id, FirstName = firstName, LastName=lastName, Salary = salary };
-                    break;
-                case EmployeeType.DeputyHeadMaster:
-                    employee = FactoryPattern<IEmployee, DeputyHeadMaster>.GetInstance();
-                        //new HeadOfDepartment { Id = id, FirstName = firstName, LastName = lastName, Salary = salary };
-                    break;
-                case EmployeeType.HeadMaster:
-                    employee = FactoryPattern<IEmployee, HeadMaster>.GetInstance();
-                    //new HeadOfDepartment { Id = id, FirstName = firstName, LastName = lastName, Salary = salary };
-                    break;
-                default:
-                    break;
-            }
-            if (employee != null)
-            {
-                employee.Id = id;
-                employee.FirstName = firstName;
-                employee.LastName = lastName;
-                employee.Salary = salary;
-            }
-            else
-            {
-                throw new NullReferenceException();
-            }
-
-            return employee;
-        }
-    }
 }
