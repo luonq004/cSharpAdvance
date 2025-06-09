7df0ed92df98912616283e19bd8114928b95eb64
Luong <luongvq004@gmail.com>
2025-06-09 13:58:02 +0700

    learn basic

--- SchoolHRAdministration/Program.cs
+++ SchoolHRAdministration/Program.cs
@@ -2,13 +2,45 @@
 {
     class Program
     {
+        delegate void LogDel(string mess);
+
         static void Main(string[] args)
         {
-            
+            Log log = new Log();
+
+            //LogDel logDel = new LogDel(log.LogTextToFile
+            //logDel("Linog");
+
+            LogDel LogTextToScreenDel, LogTextToFileDel;
+            LogTextToScreenDel = new LogDel(log.LogTextToScreen);
+
+            LogText(LogTextToScreenDel, "Luong");
+
+            Console.ReadKey();
+        }
+
+        static void LogText(LogDel logDel, string text)
+        {
+            logDel(text);
         }
 
        
     }
 
+    public class Log
+    {
+        public void LogTextToScreen(string mess)
+        {
+            Console.WriteLine($"{DateTime.Now}: {mess}");
+        }
+
+        public void LogTextToFile(string mess)
+        {
+            using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.txt"), true))
+            {
+                sw.WriteLine($"{DateTime.Now}: {mess}");
+            }
+        }
+    }
 
 }
